31f37001190e69deb6c3ccc0a57a815f82842997
Joshua <bluedude227@gmail.com>
2018-02-15 18:15:11 -0600

    Added file streaming argument

--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 IP=$1
-SLEEP=$2
+FILE=$2
 
 if [ -z $1 ]
 then
@@ -7,8 +7,10 @@ then
     exit
 fi
 
-if [ $SLEEP = 'y' ]
-then sleep 2
+if [ -z $2 ]
+then
+    gst-launch-1.0 alsasrc device=hw:1 ! udpsink host=$1 port=9001
+else
+    gst-launch-1.0 -v filesrc location=$FILE ! wavparse ! udpsink host=$IP port=9001
 fi
 
-gst-launch-1.0 alsasrc device=hw:1 ! udpsink host=$1 port=9001
